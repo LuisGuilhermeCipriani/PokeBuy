2fb58f60b50c7ba9861f45a3fce21c14d72198e2
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2022-04-12 19:51:07 -0300

    Cria classe Pokemon

--- src/controller/PokeBuy.java
+++ src/controller/PokeBuy.java
@@ -3,18 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package controller;
+
+import java.util.List;
+import model.Pokemon;
 
 /**
  *
  * @author cipri
  */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+public class PokeBuy {
+    private List<Pokemon> pokemonArmazenados;
+   
 }
---
+++ src/model/Pokemon.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author cipri
+ */
+public class Pokemon {
+    private String nome = "";
+    private String tipo = "";
+    private Float preco;
+    private String som = "";
+    
+    Pokemon(){
+        
+    };
+    
+    Pokemon(String nome, String tipo, Float preco, String som){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.preco = preco;
+        this.som = som;
+    };
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+
+    public String getSom() {
+        return som;
+    }
+
+    public void setSom(String som) {
+        this.som = som;
+    }
+    
+    
+}
